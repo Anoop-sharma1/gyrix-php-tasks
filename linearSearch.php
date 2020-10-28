2dae2b184d2b14cac3b608195c7d2ffdddf46191
Anoop Sharma <52255479+Anoop-sharma1@users.noreply.github.com>
2020-10-28 14:09:29 +0530

    Update linearSearch.php

--- linearSearch.php
+++ linearSearch.php
@@ -1,18 +1,18 @@
 <?php 
-    $array = array(7 , 6 , 4 , 5 , 8 , 2 , 1);
+    $array = array (7 , 6 , 4 , 5 , 8 , 2 , 1);
     $count = 0;
     $searchElement = 5;
     $arrayIndex = 0;
 
-    if(is_int($searchElement)) {
-        foreach($array as $index => $value) {
-            if($searchElement == $value) {
+    if (is_int($searchElement)) {
+        foreach ($array as $index => $value) {
+            if ($searchElement == $value) {
                 $count++;
                 $arrayIndex = $index;
             } 
         }
     
-        if($count >= 1) {
+        if ($count >= 1) {
             echo "Element Found at index[$arrayIndex] == $searchElement ";
         }
     
