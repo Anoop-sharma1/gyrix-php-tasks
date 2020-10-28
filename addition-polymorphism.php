efda5179cb209e72ec373ab39c53622b417b93ee
Anoop Sharma <52255479+Anoop-sharma1@users.noreply.github.com>
2020-10-28 14:08:26 +0530

    Update addition-polymorphism.php

--- addition-polymorphism.php
+++ addition-polymorphism.php
@@ -1,7 +1,7 @@
 <?php
     function addition($num1 ,$num2) {
 
-        if(is_int($num1) == 1 && is_int($num2) == 1 ) {
+        if (is_int($num1) == 1 && is_int($num2) == 1 ) {
             return $num1 + $num2;
         }
 
@@ -17,27 +17,27 @@
             return $num1 + $num2;
         }
 
-        else if(is_string($num1) == 1 && is_string($num2) == 1) {
+        else if (is_string($num1) == 1 && is_string($num2) == 1) {
             return $num1 ." ". $num2;
         }
 
-        else if(is_string($num1) == 1 && is_int($num2) == 1) {
+        else if (is_string($num1) == 1 && is_int($num2) == 1) {
             return $num1 ." ". $num2;
         }
 
-        else if(is_float($num1) == 1 && is_string($num2) == 1) {
+        else if (is_float($num1) == 1 && is_string($num2) == 1) {
             return $num1 ." ". $num2;
         }
 
-        else if(is_string($num1) == 1 && is_float($num2) == 1) {
+        else if (is_string($num1) == 1 && is_float($num2) == 1) {
             return $num1 ." ". $num2;
         }
 
-        else if(is_int($num1) == 1 && is_string($num2) == 1) {
+        else if (is_int($num1) == 1 && is_string($num2) == 1) {
             return $num1 ." ". $num2;
         }
 
-        else if(is_string($num1) == 1 && is_int($num2) == 1) {
+        else if (is_string($num1) == 1 && is_int($num2) == 1) {
             return $num1 ." ". $num2;
         }
 
