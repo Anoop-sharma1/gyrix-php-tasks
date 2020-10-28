bd82b19c4090ad47184cee4165ba0b3dc82078aa
Gyrix19 <Anoop-sharma1>
2020-10-28 13:58:26 +0530

    Linear Search on array in Php

---
+++ linearSearch.php
@@ -0,0 +1,27 @@
+<?php 
+    $array = array(7 , 6 , 4 , 5 , 8 , 2 , 1);
+    $count = 0;
+    $searchElement = 5;
+    $arrayIndex = 0;
+
+    if(is_int($searchElement)){
+        foreach($array as $index => $value) {
+            if($searchElement == $value) {
+                $count++;
+                $arrayIndex = $index;
+            } 
+        }
+    
+        if($count >= 1) {
+            echo "Element Found at index[$arrayIndex] == $searchElement ";
+        }
+    
+        else {
+            echo "Element Not Found !!";
+        }
+    }
+
+    else {
+        echo "Invalid Entry !!";
+    }  
+?>
